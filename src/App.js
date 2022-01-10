53b1423813fbfa33e10cef6cac75e7e08ad53bc7
MykytaPervushyn <80350205+MykytaPervushyn@users.noreply.github.com>
2022-01-10 07:18:33 +0200

    зберігає зміни

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
-// import ContactForm from './components/ContactForm';
-// import ContactList from './components/ContactList';
-// import Filter from './components/Filter';
 import { useDispatch, useSelector } from 'react-redux';
 import AppBar from './components/AppBar/AppBar';
-import ContactsView from './views/ContactsView';
-import HomeView from './views/HomeView';
-import RegisterView from './views/RegisterView';
-import LoginView from './views/LoginView';
 import Container from './components/Container';
-// import { useFetchContactsQuery, } from './redux/contacts/contactsSlice';
-
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
 import { useEffect, lazy, Suspense } from 'react';
@@ -22,19 +13,6 @@ import { authOperations, authSelectors } from './redux/auth';
 
 
 export default function App() {
-//   const [filter, setFilter] = useState('');
-
-//   const { data: contacts, isFetching } = useFetchContactsQuery();
-//   console.log(contacts);
-
-//   const getFiltredContacts = data =>
-//     data.filter(contact =>
-//       contact.name.toLowerCase().includes(filter.toLowerCase()),
-//     );
-
-//   const handleChange = e => {
-//     setFilter(e.target.value)
-//   }
   
   const dispatch = useDispatch();
   const isFetchingCurrentUser = useSelector(authSelectors.getFetchingCurrent);
@@ -58,12 +36,6 @@ export default function App() {
 
   return (
     <Container>
-      {/* <h1>Phonebook</h1>
-      <ContactForm contacts={contacts}/>
-      <h2>Contacts</h2>
-      <Filter onChange={handleChange} />
-      {!isFetching && <ContactList contacts={getFiltredContacts(contacts)}
-      />} */}
       {isFetchingCurrentUser ? (
         <Audio
           heigth="300"
--- src/views/ContactsView.js
+++ src/views/ContactsView.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Container from '../components/Container';
 import ContactList from '../components/ContactList/ContactList';
@@ -8,8 +8,6 @@ import { contactsOperations, contactsSelectors } from '../redux/contacts';
 import s from './Views.module.css';
 
 const barStyles = {
-  // display: 'flex',
-  // alignItems: 'flex-end',
   marginBottom: 20,
 };
 
